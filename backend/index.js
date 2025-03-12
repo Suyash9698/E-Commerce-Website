583bf3eb7bf3fc0ff536c26c608394189007f133
Suyash Khare <52395988+Suyash9698@users.noreply.github.com>
2025-03-12 22:32:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL.replace(/\/$/, ""), // Removes trailing slash
+    origin: "https://e-commerce-website-one-jet.vercel.app", // Removes trailing slash
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
