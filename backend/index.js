19fa0d40c994af5b6b8cc14557918e8da8189925
Suyash Khare <52395988+Suyash9698@users.noreply.github.com>
2025-03-12 11:40:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const connectDB = require('./config/db');
-const router = require('./routes');
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
 
-const app = express();
 
+const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL.replace(/\/$/, ""),
+    origin: process.env.FRONTEND_URL.replace(/\/$/, ""), // Removes trailing slash
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.use(express.json({ limit: "50mb" }));
-app.use(cookieParser());
-app.use("/api", router);
+app.use(express.json({ limit: "50mb" }))
+app.use(cookieParser())
+app.use("/api",router)
 
-const server = async (req, res) => {
-    await connectDB();
-    return app(req, res);
-};
+const PORT = 8080 || process.env.PORT
 
-module.exports = server; // ✅ Exporting function for Vercel
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
