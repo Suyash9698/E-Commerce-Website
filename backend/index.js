3487e5b3917847c9f0f34f7f7badb9815ce6c8ec
Suyash Khare <52395988+Suyash9698@users.noreply.github.com>
2025-03-12 22:01:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const connectDB = require('./config/db');
-const router = require('./routes');
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
 
-const app = express();
+
+const app = express()
 app.use(cors({
     origin: process.env.FRONTEND_URL.replace(/\/$/, ""), // Removes trailing slash
     credentials: true,
@@ -13,14 +14,16 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.use(express.json({ limit: "50mb" }));
-app.use(cookieParser());
-app.use("/api", router);
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api",router)
 
-// Connect to DB before handling requests (optional based on your use case)
-connectDB().catch(err => {
-  console.error("Database connection failed:", err);
-});
+const PORT = process.env.PORT || 9001
 
-// Export the app for Vercel
-module.exports = app;
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
