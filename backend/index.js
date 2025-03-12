8626eca6376f633ca2dde66b92a42ca50c6d095e
Suyash Khare <52395988+Suyash9698@users.noreply.github.com>
2025-03-12 21:43:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 app.use("/api",router)
 
-const PORT = process.env.PORT || 8080
+// const PORT = process.env.PORT || 8080
+const PORT = 8001
 
 
 connectDB().then(() => {
